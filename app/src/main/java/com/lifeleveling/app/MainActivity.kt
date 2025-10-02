d6a8a15992d3cc4c7c58cee33d8b130539e004b7
StephenC1993 <stephen.conn1993@gmail.com>
2025-10-02 13:56:00 -0400

    Drawables added to /resources for icons

--- app/src/main/java/com/lifeleveling/app/MainActivity.kt
+++ app/src/main/java/com/lifeleveling/app/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -47,4 +48,5 @@ fun GreetingPreview() {
     LifelevelingTheme {
         Greeting("Android")
     }
+
 }
