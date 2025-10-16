98eca3948a941ff0ae4a0f81bf13b5e7a2b607b9
Elyseia <166407679+Elyseia@users.noreply.github.com>
2025-10-16 12:41:50 -0600

    TestUser object created

---
+++ app/src/main/java/com/lifeleveling/app/ui/screens/TestUser.kt
@@ -0,0 +1,21 @@
+package com.lifeleveling.app.ui.screens
+
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.getValue
+
+/*
+* This is just a singleton test user for us to hardcode values into for UI testing
+* Later this can be used to build the logic needed for each user account
+* as well as the local save of user logic.
+*/
+
+object TestUser {
+    var name by mutableStateOf("John Doe")
+    var level by mutableStateOf(3)
+    var currentExp by mutableStateOf(280)
+    var expToLevel by mutableStateOf(300)
+    var coins by mutableStateOf(301)
+    var currentHealth by mutableStateOf(34)
+    var maxHealth by mutableStateOf(85)
+}
