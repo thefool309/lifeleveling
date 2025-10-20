c7f4cb6302dda09cb1fe302a59338c929bce330c
Elyseia <166407679+Elyseia@users.noreply.github.com>
2025-10-20 14:54:29 -0600

    Reusable tooltip builder created

--- app/src/main/java/com/lifeleveling/app/ui/theme/ToolTips.kt
+++ app/src/main/java/com/lifeleveling/app/ui/theme/ToolTips.kt
@@ -6,11 +6,9 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
@@ -20,7 +18,6 @@ import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Shadow
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.AnnotatedString
-import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.dp
@@ -60,37 +57,19 @@ fun BulletPoints(items: List<AnnotatedString>) {
     }
 }
 
-
 /**
- * Level and Experience Popup Tool Tip
- * @param toShow The boolean to toggle if it is shown
+ * Saving the dialog options with parameters to pass in making tooltips look the same
+ * @param toShow Boolean for showing the dialog
+ * @param title Title of tooltip window. Call as R.string.[string name]
+ * @param tips Build a list of annotatedStrings to pass in as bullet points
  */
 @Composable
-fun LifeExperienceToolTip(toShow: MutableState<Boolean>) {
-    // Level and Experience tips
-    val LevelTips = listOf(
-        buildAnnotatedString {
-            withStyle(style = AppTheme.textStyles.Small.toSpanStyle().copy(color = AppTheme.colors.Gray)) {
-                append(stringResource(R.string.life_tip_one))
-            }
-        },
-        buildAnnotatedString {
-            withStyle(style = AppTheme.textStyles.Small.toSpanStyle().copy(color = AppTheme.colors.Gray)) {
-                append("The bar shows your experience progress toward your next level.")
-            }
-        },
-        buildAnnotatedString {
-            withStyle(style = AppTheme.textStyles.Small.toSpanStyle().copy(color = AppTheme.colors.Gray)) {
-                append("You earn experience idly while your character is fighting.")
-            }
-        },
-        buildAnnotatedString {
-            withStyle(style = AppTheme.textStyles.Small.toSpanStyle().copy(color = AppTheme.colors.Gray)) {
-                append("Experience earned is based on your stats.")
-            }
-        }
-    )
-
+fun Tooltip(
+    modifier: Modifier = Modifier,
+    toShow: MutableState<Boolean>,
+    title: Int,
+    tips: List<AnnotatedString>
+) {
     Dialog(
         onDismissRequest = { toShow.value = false },
         properties = DialogProperties(
@@ -99,7 +78,7 @@ fun LifeExperienceToolTip(toShow: MutableState<Boolean>) {
     ) {
         // Making a custom dim
         Box (
-            modifier = Modifier
+            modifier = modifier
                 .fillMaxSize()
                 .background(color = AppTheme.colors.DarkerBackground.copy(alpha = 0.1f))
                 .clickable { toShow.value = false },
@@ -117,7 +96,7 @@ fun LifeExperienceToolTip(toShow: MutableState<Boolean>) {
                 ) {
                     // Title
                     Text(
-                        stringResource(R.string.life_tip_title),
+                        stringResource(title),
                         color = AppTheme.colors.SecondaryThree,
                         style = AppTheme.textStyles.HeadingSix.copy(
                             shadow = Shadow(
@@ -128,58 +107,80 @@ fun LifeExperienceToolTip(toShow: MutableState<Boolean>) {
                         )
                     )
                     // Content
-                    BulletPoints(LevelTips)
+                    BulletPoints(tips)
                 }
             }
         }
     }
 }
 
-@Composable
-fun HealthToolTip(toShow: MutableState<Boolean>) {
-    // Level and Experience tips
-    val firstHealthTip = buildAnnotatedString {}
 
-    Dialog(
-        onDismissRequest = { toShow.value = false },
-        properties = DialogProperties(
-            usePlatformDefaultWidth = false
-        )
-    ) {
-        // Making a custom dim
-        Box (
-            modifier = Modifier
-                .fillMaxSize()
-                .background(color = AppTheme.colors.DarkerBackground.copy(alpha = 0.1f))
-                .clickable { toShow.value = false },
-        ) {
-            PopupCard(
-                modifier = Modifier
-                    .align(Alignment.Center)
-                    .clickable { toShow.value = false }
-            ) {
-                Column(
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                    verticalArrangement = Arrangement.spacedBy(8.dp)
-                ) {
-                    Text(
-                        "Example Popup",
-                        color = AppTheme.colors.SecondaryThree,
-                        style = AppTheme.textStyles.HeadingFour.copy(
-                            shadow = Shadow(
-                                color = AppTheme.colors.DropShadow,
-                                offset = Offset(3f, 4f),
-                                blurRadius = 6f,
-                            )
-                        )
-                    )
-                    Text(
-                        "Testing popup capabilities and text wrapping. Click me to close.",
-                        color = AppTheme.colors.Gray,
-                        style = AppTheme.textStyles.Default
-                    )
-                }
+/**
+ * Level and Experience Popup Tool Tip
+ * @param toShow The boolean to toggle if it is shown
+ */
+@Composable
+fun LifeExperienceToolTip(toShow: MutableState<Boolean>) {
+    // Bullet Points
+    val levelTips = listOf(
+        buildAnnotatedString {
+            withStyle(style = AppTheme.textStyles.Small.toSpanStyle().copy(color = AppTheme.colors.Gray)) {
+                append(stringResource(R.string.life_tip_one))
+            }
+        },
+        buildAnnotatedString {
+            withStyle(style = AppTheme.textStyles.Small.toSpanStyle().copy(color = AppTheme.colors.Gray)) {
+                append(stringResource(R.string.life_tip_two))
+            }
+        },
+        buildAnnotatedString {
+            withStyle(style = AppTheme.textStyles.Small.toSpanStyle().copy(color = AppTheme.colors.Gray)) {
+                append(stringResource(R.string.life_tip_three))
+            }
+        },
+        buildAnnotatedString {
+            withStyle(style = AppTheme.textStyles.Small.toSpanStyle().copy(color = AppTheme.colors.Gray)) {
+                append(stringResource(R.string.life_tip_four))
             }
         }
-    }
+    )
+
+    // Dialog window
+    Tooltip(
+        toShow = toShow,
+        title = R.string.life_tip_title,
+        tips = levelTips
+    )
+}
+
+@Composable
+fun HealthToolTip(toShow: MutableState<Boolean>) {
+    // Bullet Points
+    val healthTips = listOf(
+        buildAnnotatedString {
+            withStyle(style = AppTheme.textStyles.SmallUnderlined.toSpanStyle().copy(color = AppTheme.colors.SecondaryThree)) {
+                append(stringResource(R.string.health))
+            }
+            withStyle(style = AppTheme.textStyles.Small.toSpanStyle().copy(color = AppTheme.colors.Gray)) {
+                append(stringResource(R.string.health_tip_one))
+            }
+        },
+        buildAnnotatedString {
+            withStyle(style = AppTheme.textStyles.Small.toSpanStyle().copy(color = AppTheme.colors.Gray)) {
+                append(stringResource(R.string.health_tip_two))
+            }
+        },
+        buildAnnotatedString {
+            withStyle(style = AppTheme.textStyles.Small.toSpanStyle().copy(color = AppTheme.colors.Gray)) {
+                append(stringResource(R.string.health_tip_three))
+            }
+        }
+    )
+
+    // Dialog window
+    Tooltip(
+        toShow = toShow,
+        title = R.string.health_tip_title,
+        tips = healthTips
+    )
 }
