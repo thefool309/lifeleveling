d3e5537e95c6533840d0ae19cce94479019bdf04
lipeduh <fdesouza@student.fullsail.edu>
2025-10-31 18:21:09 -0400

    Included a companion object with a singleton to be used safely in Stats-Screen.kt

--- app/src/main/java/com/lifeleveling/app/util/ILogger.kt
+++ app/src/main/java/com/lifeleveling/app/util/ILogger.kt
@@ -6,4 +6,14 @@ interface ILogger {
     fun e(tag: String, message: String, throwable: Throwable)
     fun w(tag: String, message: String)
     fun i(tag: String, message: String)
+
+    companion object{
+        val DEFAULT: ILogger = object : ILogger {
+            override fun d(tag: String, message: String) {}
+            override fun e(tag: String, message: String) {}
+            override fun e(tag: String, message: String, throwable: Throwable) {}
+            override fun w(tag: String, message: String) {}
+            override fun i(tag: String, message: String) {}
+        }
+    }
 }
