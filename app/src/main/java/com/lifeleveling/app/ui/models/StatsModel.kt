9a1c07a0a3d01c1021b3d5248a43b6bdc7342766
lipeduh <fdesouza@student.fullsail.edu>
2025-11-05 21:19:18 -0500

    Refactoring the Stats Screen backend connection by introducing StatsUi and EditedStats UI models in the new file StatsModel.kt

---
+++ app/src/main/java/com/lifeleveling/app/ui/models/StatsModel.kt
@@ -0,0 +1,24 @@
+package com.lifeleveling.app.ui.models
+
+data class StatsUi(
+    val level: Int,
+    val currentXp: Int,
+    val xpToNext: Int,
+    val usedLifePoints: Int,                // already-allocated points (sum of stats)
+    val unusedLifePoints: Int,              // total pool shown on the right of "used/total"
+    val strength: Int,
+    val defense: Int,
+    val intellect: Int,
+    val agility: Int,
+    val health: Int
+)
+
+data class EditedStats(
+    val strength: Int,
+    val defense: Int,
+    val intellect: Int,
+    val agility: Int,
+    val health: Int,
+    val usedPoints: Int,
+    val remainingPoints: Int                 // this should be the new lifePoints to persist
+)
