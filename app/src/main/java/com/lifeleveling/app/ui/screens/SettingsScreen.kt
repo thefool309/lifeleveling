6863c5380a501aecbca44742ad4da1a569de6c5a
Elyseia <166407679+Elyseia@users.noreply.github.com>
2025-11-10 13:57:54 -0700

    import cleanup

--- app/src/main/java/com/lifeleveling/app/ui/screens/SettingsScreen.kt
+++ app/src/main/java/com/lifeleveling/app/ui/screens/SettingsScreen.kt
@@ -1,7 +1,6 @@
 package com.lifeleveling.app.ui.screens
 
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -35,8 +34,6 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.draw.scale
-import androidx.compose.ui.layout.ModifierLocalBeyondBoundsLayout
 import androidx.compose.ui.res.stringResource
 import com.lifeleveling.app.ui.components.SeparatorLine
 
