6c379868281e8bf215965d758567010108b333fd
Elyseia <166407679+Elyseia@users.noreply.github.com>
2025-11-19 13:03:55 -0700

    Fixed problem from my fix...

--- app/src/main/java/com/lifeleveling/app/ui/components/CustomBaseComponents.kt
+++ app/src/main/java/com/lifeleveling/app/ui/components/CustomBaseComponents.kt
@@ -1279,7 +1279,7 @@ fun CustomTextField(
             )
         },
         keyboardOptions = keyboardOptions,
-        supportingText = if (supportingUnit != null) { { supportingUnit } } else null,
+        supportingText = if (supportingUnit != null) { { supportingUnit.invoke() } } else null,
         visualTransformation = visualTransformation,
     )
 }
