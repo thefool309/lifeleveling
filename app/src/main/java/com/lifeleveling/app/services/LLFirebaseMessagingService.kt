9dab77466e13ca910cb4bc758938e65acd53f2c4
thefool309 <themagician309@gmail.com>
2025-11-25 14:48:01 -0600

    - added strings to strings.xml for use in the Notification Settings in android. - Fixed AndroidManifest.xml so the project would build - Created LLFirebaseMessagingService.kt

---
+++ app/src/main/java/com/lifeleveling/app/services/LLFirebaseMessagingService.kt
@@ -0,0 +1,11 @@
+package com.lifeleveling.app.services
+
+import com.google.firebase.messaging.FirebaseMessagingService
+
+class LLFirebaseMessagingService : FirebaseMessagingService() {
+
+
+    fun onNewToken() {
+
+    }
+}
