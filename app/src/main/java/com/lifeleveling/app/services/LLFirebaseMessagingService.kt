0327ba8c2a2b562a2d5c7bc71f3361a2593c8502
thefool309 <themagician309@gmail.com>
2025-11-25 16:54:48 -0600

    - created onMessageReceived for our LLFirebaseMessagingService.kt - going to do more reading on services in android.

--- app/src/main/java/com/lifeleveling/app/services/LLFirebaseMessagingService.kt
+++ app/src/main/java/com/lifeleveling/app/services/LLFirebaseMessagingService.kt
@@ -1,9 +1,31 @@
 package com.lifeleveling.app.services
 
+import androidx.core.app.NotificationCompat
 import com.google.firebase.messaging.FirebaseMessagingService
-
+import com.google.firebase.messaging.RemoteMessage
+import com.lifeleveling.app.R
 class LLFirebaseMessagingService : FirebaseMessagingService() {
 
+    val CHANNEL_ID = "com.lifeleveling.app.FirebaseMessagingService"
+
+
+    /**
+     * handles recieving the message from firebase, and creating and displaying the notification
+     * @param message The `RemoteMessage` from firebase
+     * @see FirebaseMessagingService.onMessageReceived
+     * @see android.app.Service
+     */
+    override fun onMessageReceived(message: RemoteMessage) {
+        super.onMessageReceived(message)
+
+        // create the NotificationCompat.Builder
+        val builder = NotificationCompat.Builder(applicationContext, CHANNEL_ID)
+            .setSmallIcon(R.drawable.ic_stat_name)
+            .setContentTitle(message.data["title"])
+            .setContentText(message.data["message"])
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .setAutoCancel(true)
+    }
 
     fun onNewToken() {
 
