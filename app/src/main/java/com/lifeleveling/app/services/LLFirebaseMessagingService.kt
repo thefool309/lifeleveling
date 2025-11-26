429919a533824b61647b309667b3bf9bd781965f
thefool309 <themagician309@gmail.com>
2025-11-25 18:17:17 -0600

    - added logging statements for debugging the message content

--- app/src/main/java/com/lifeleveling/app/services/LLFirebaseMessagingService.kt
+++ app/src/main/java/com/lifeleveling/app/services/LLFirebaseMessagingService.kt
@@ -4,30 +4,44 @@ import androidx.core.app.NotificationCompat
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 import com.lifeleveling.app.R
-class LLFirebaseMessagingService : FirebaseMessagingService() {
+import com.lifeleveling.app.util.ILogger
+
+/**
+ *
+ * @param message The `RemoteMessage` from firebase
+ * @see FirebaseMessagingService.onMessageReceived
+ * @see android.app.Service
+ */
+class LLFirebaseMessagingService(val logger: ILogger) : FirebaseMessagingService() {
 
     val CHANNEL_ID = "com.lifeleveling.app.FirebaseMessagingService"
+    val TAG = "FirebaseMessagingService"
+
 
 
     /**
-     * handles recieving the message from firebase, and creating and displaying the notification
+     * handles receiving the message from firebase, and creating and displaying the notification
      * @param message The `RemoteMessage` from firebase
      * @see FirebaseMessagingService.onMessageReceived
      * @see android.app.Service
      */
     override fun onMessageReceived(message: RemoteMessage) {
         super.onMessageReceived(message)
+        if(com.lifeleveling.app.BuildConfig.DEBUG) {
+            logger.d(TAG, "Message ID: " + message.messageId)
+        }
+        if (message.data.isNotEmpty()) {
 
-        // create the NotificationCompat.Builder
-        val builder = NotificationCompat.Builder(applicationContext, CHANNEL_ID)
-            .setSmallIcon(R.drawable.ic_stat_name)
-            .setContentTitle(message.data["title"])
-            .setContentText(message.data["message"])
-            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
-            .setAutoCancel(true)
-    }
-
-    fun onNewToken() {
-
+            if(com.lifeleveling.app.BuildConfig.DEBUG) {
+                logger.d(TAG, "Message data payload: " + message.data)
+            }
+            // create the NotificationCompat.Builder
+            val builder = NotificationCompat.Builder(applicationContext, CHANNEL_ID)
+                .setSmallIcon(R.drawable.ic_stat_name)
+                .setContentTitle(message.data["title"])
+                .setContentText(message.data["message"])
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setAutoCancel(true)
+        }
     }
 }
