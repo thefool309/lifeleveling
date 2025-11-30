a8d2dfc31cf76f6ca375c27ff92b31e2a981dc3e
thefool309 <themagician309@gmail.com>
2025-11-30 17:34:45 -0600

    feature: implemented the logic of creating the notification and sending it - remains untested - needs to be worked into main activity - following the quickstart-android guide on github https://github.com/firebase/quickstart-android/blob/0b77e46545ef38b040d140f360cfe3f75b54e15d/messaging/app/src/main/java/com/google/firebase/quickstart/fcm/kotlin/MyFirebaseMessagingService.kt

--- app/src/main/java/com/lifeleveling/app/services/LLFirebaseMessagingService.kt
+++ app/src/main/java/com/lifeleveling/app/services/LLFirebaseMessagingService.kt
@@ -1,8 +1,15 @@
 package com.lifeleveling.app.services
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.media.RingtoneManager
 import androidx.core.app.NotificationCompat
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
+import com.lifeleveling.app.MainActivity
 import com.lifeleveling.app.R
 import com.lifeleveling.app.data.FirestoreRepository
 import com.lifeleveling.app.util.AndroidLogger
@@ -13,8 +20,8 @@ import kotlinx.coroutines.runBlocking
  * A service for handling Firebase Cloud Messaging interactions will handle receiving the messages and sending the notifications
  * Override base class methods to handle any events required by the application. All methods are invoked on a background thread,
  * and may be called when the app is in the background or not open
- * @property channelID and arbitrary channelID I selected for the notification channel
- * @property logTag the tag for the logger to point you back to this file
+ * @property CHANNEL_ID and arbitrary channelID I selected for the notification channel
+ * @property TAG the tag for the logger to point you back to this file
  * @property repo a `FirestoreRepository()` object for saving FcmTokens
  * @see FirebaseMessagingService
  * @see android.app.Service
@@ -22,12 +29,11 @@ import kotlinx.coroutines.runBlocking
  * @author thefool309
  */
 class LLFirebaseMessagingService() : FirebaseMessagingService() {
-
-
     var logger: ILogger = AndroidLogger()
-    val channelID = "LifeLevelingFirebaseMessagingService"
-    val logTag = "FirebaseMessagingService"
-
+companion object {
+    const val CHANNEL_ID = "LifeLevelingFirebaseMessagingService"
+    const val TAG = "FirebaseMessagingService"
+}
     val repo = FirestoreRepository()
 
     constructor(logger: ILogger) : this() {
@@ -44,28 +50,23 @@ class LLFirebaseMessagingService() : FirebaseMessagingService() {
     override fun onMessageReceived(message: RemoteMessage) {
         super.onMessageReceived(message)
         if(com.lifeleveling.app.BuildConfig.DEBUG) {
-            logger.d(logTag, "Message ID: " + message.messageId)
+            logger.d(TAG, "Message ID: " + message.messageId)
         }
         if (message.data.isNotEmpty()) {
 
             if(com.lifeleveling.app.BuildConfig.DEBUG) {
-                logger.d(logTag, "Message data payload: " + message.data)
+                logger.d(TAG, "Message data payload: " + message.data)
             }
-            // create the NotificationCompat.Builder
-            val builder = NotificationCompat.Builder(applicationContext, channelID)
-                .setSmallIcon(R.drawable.ic_stat_name)
-                .setContentTitle(message.data["title"])
-                .setContentText(message.data["message"])
-                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
-                .setAutoCancel(true)
         }
         // Check if message contains a notification payload.
         message.notification?.let {
-            logger.d(logTag, "Message Notification Body: ${it.body}")
+            logger.d(TAG, "Message Notification Body: ${it.body}")
+            it.body?.let { body: String -> sendNotification(body) }
         }
+
     }
     override fun onNewToken(token: String) {
-        logger.d(logTag, "Refreshed token: $token")
+        logger.d(TAG, "Refreshed token: $token")
 
         // If you want to send messages to this application instance or
         // manage this apps subscriptions on the server side, send the
@@ -75,6 +76,46 @@ class LLFirebaseMessagingService() : FirebaseMessagingService() {
     private fun sendRegistrationToServer(token: String?) {
         // send token to the Firestore.
         runBlocking {   repo.setFirebaseToken(token, logger) }
-        logger.d(logTag, "sendRegistrationTokenToServer($token)")
+        logger.d(TAG, "sendRegistrationTokenToServer($token)")
+    }
+
+    /**
+     * the function that carries out the logic  of creating the notification and sending it.
+     *
+     */
+
+    private fun sendNotification(messageBody: String) {
+        val intent = Intent(this, MainActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+        val pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_IMMUTABLE)
+        
+        val channelId = CHANNEL_ID
+        val notificationBuilder = NotificationCompat.Builder(applicationContext, CHANNEL_ID)
+            .setSmallIcon(R.drawable.ic_stat_name)
+            .setContentText(messageBody)
+            .setSound(RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION))
+            .setAutoCancel(true)
+            .setContentIntent(pendingIntent)
+            .setContentTitle() //must be placed at the end since we overrode it's local functionality
+
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        if(android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(CHANNEL_ID, channelId, NotificationManager.IMPORTANCE_DEFAULT)
+            notificationManager.createNotificationChannel(channel)
+        }
+        notificationManager.notify(0, notificationBuilder.build())
     }
 }
+
+/**
+ * extension function for NotificationCompat.Builder that gives us a default ContentTitle for notifications.
+ * must be placed last on the NotificationCompat.Builder() if using the default,
+ * so we should make it a habit to always place anything we extend last
+ * @see LLFirebaseMessagingService.sendNotification
+ */
+private fun NotificationCompat.Builder.setContentTitle() : NotificationCompat.Builder {
+    setContentTitle("Life Leveling")
+    return this
+}
+
