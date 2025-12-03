c35558bb140dc951a74cac24ace25c5ca10faaaf
thefool309 <themagician309@gmail.com>
2025-12-02 22:53:02 -0600

    Feature: local notifications  added comments to ReminderScheduler.kt

--- app/src/main/java/com/lifeleveling/app/services/ReminderScheduler.kt
+++ app/src/main/java/com/lifeleveling/app/services/ReminderScheduler.kt
@@ -5,15 +5,21 @@ import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
 import android.icu.util.Calendar
+import com.lifeleveling.app.BuildConfig
 import com.lifeleveling.app.data.Reminders
+import com.lifeleveling.app.util.AndroidLogger
+import com.lifeleveling.app.util.ILogger
 
 
-class ReminderScheduler(private val context: Context) {
+class ReminderScheduler(private val context: Context, val logger: ILogger = AndroidLogger()) {
     /**
      * Schedule a reminder. Takes in the reminder and handles the rest of the legwork for you
      * can be used to Schedule a new reminder, or to
      * @param reminder the reminder to have a notification scheduled for it
      */
+    companion object {
+        const val TAG = "ReminderScheduler"
+    }
     fun schedule(reminder: Reminders) {
         val intent = Intent(context, ReminderReceiver::class.java).apply {
             putExtra("title", reminder.title)
@@ -26,6 +32,7 @@ class ReminderScheduler(private val context: Context) {
             intent,
              PendingIntent.FLAG_IMMUTABLE or PendingIntent.FLAG_UPDATE_CURRENT
         )
+        if (BuildConfig.DEBUG) { logger.d(TAG, "Reminder intent created for ${reminder.title}") }
         val alarmManager = context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
         // an example of how to set it to go off at a specific date.
         val calendar = Calendar.getInstance().apply {
@@ -36,15 +43,17 @@ class ReminderScheduler(private val context: Context) {
             add(Calendar.DAY_OF_MONTH, 1)
         }
 
-        val triggerAt = calendar.timeInMillis
+        val triggerAt = /*calendar.timeInMillis*/ // uncomment this for the calender defined time
+            System.currentTimeMillis() + 10_000 // uncomment this for ten seconds from now
 
         alarmManager.setExactAndAllowWhileIdle(
             AlarmManager.RTC_WAKEUP,
             // triggerAtMillis uses the same time format as Java/Kotlin timestamps us everywhere.
             // Millis since the Unix epoch (Unix Standard Time in milliseconds)
-            System.currentTimeMillis() + 10_000, // TODO: find out how to get the timestamps we need to remindAt
+            triggerAt, // TODO: find out how to get the timestamps we need to remindAt
             pendingIntent
         )
+        if(BuildConfig.DEBUG) { logger.d(TAG, "Reminder scheduled to ${reminder.title} at $triggerAt") }
     }
 
     fun cancel(reminder: Reminders) {
@@ -58,5 +67,11 @@ class ReminderScheduler(private val context: Context) {
         val alarmManager = context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
         alarmManager.cancel(pendingIntent)
 
+        if(BuildConfig.DEBUG) {
+            logger.d(
+                TAG,
+                "Reminder cancelled: reminderId ${reminder.reminderId}: reminder name ${reminder.title}"
+            )
+        }
     }
 }
