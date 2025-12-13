26663332c12923c5f13c043d6d0cfdd610e61731
thefool309 <themagician309@gmail.com>
2025-12-13 06:57:39 -0600

    integration-tests: created a create-android-emu.sh script for use in our run-tests.yml workflow

---
+++ scripts/create-android-emu.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+# !! Exit immediately if a command exits with a non-zero status !!
+set -e
+
+# shellcheck disable=SC1091
+export "$(grep -v '^#' .env | xargs)"
+
+# the package path based on imported env variables
+PACKAGE="system-images;android-${API_LEVEL};google_apis;x86_64"
+
+echo " Installing android image for API level: ${API_LEVEL}"
+
+#install system image
+yes | sdkmanager "$PACKAGE"
+
+echo "Creating AVD: ${AVD_NAME} using package ${PACKAGE}"
+
+echo "no" | avdmanager create avd \
+  --force \
+  --name "${AVD_NAME}" \
+  --package "${PACKAGE}" \
+  --device "pixel_4"
+
+echo "AVD ${AVD_NAME} created successfully!"
