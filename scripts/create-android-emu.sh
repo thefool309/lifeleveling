087fbed89d391934fc4b1eb6bde0e6a409125a4f
thefool309 <themagician309@gmail.com>
2025-12-16 19:13:13 -0600

    workflows: fixed avd list command in workflow

--- scripts/create-android-emu.sh
+++ scripts/create-android-emu.sh
@@ -4,8 +4,13 @@
 set -e
 set -x
 
+AVD_NAME="TestAVD"
+
+API_LEVEL=36
 # shellcheck disable=SC1091
 # export "$(grep -v '^#' .env | xargs)" # commented out because it was causing the workflow to fail
+
+echo "$API_LEVEL"
 
 # the package path based on imported env variables
 PACKAGE="system-images;android-${API_LEVEL};google_apis;x86_64"
@@ -17,8 +22,7 @@ yes | sdkmanager "$PACKAGE"
 
 echo "Creating AVD: ${AVD_NAME} using package ${PACKAGE}"
 
-echo "yes" | avdmanager create avd \
-  -v \
+echo "yes" | avdmanager -v create avd \
   --force \
   --name "${AVD_NAME}" \
   --package "${PACKAGE}" \
